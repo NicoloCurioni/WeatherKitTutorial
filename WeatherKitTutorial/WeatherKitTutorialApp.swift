13cf3e35e6aa945f36b437c85cd8c7a724209d1c
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-08-17 14:03:23 +0200

    Initial Commit

---
+++ WeatherKitTutorial/WeatherKitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitTutorialApp.swift
+//  WeatherKitTutorial
+//
+//  Created by Nicolò Curioni on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
